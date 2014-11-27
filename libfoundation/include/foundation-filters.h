b7c0e5fc5ecba7515b00b7d83fed7f5ba1e67e3d
runrevali <ali@runrev.com>
2014-11-27 10:49:56 +0000

    [[ StdMlc ]] Tweaks to foundation and engine for std mlc library

--- libfoundation/include/foundation-filters.h
+++ libfoundation/include/foundation-filters.h
@@ -25,11 +25,6 @@
 
 bool MCFiltersBase64Decode(MCStringRef p_src, MCDataRef& r_dst);
 bool MCFiltersBase64Encode(MCDataRef p_src, MCStringRef& r_dst);
-bool MCFiltersCompress(MCDataRef p_source, MCDataRef& r_result);
-bool MCFiltersIsCompressed(MCDataRef p_source);
-bool MCFiltersDecompress(MCDataRef p_source, MCDataRef& r_result);
-bool MCFiltersIsoToMac(MCDataRef p_source, MCDataRef &r_result);
-bool MCFiltersMacToIso(MCDataRef p_source, MCDataRef &r_result);
 bool MCFiltersUrlEncode(MCStringRef p_source, MCStringRef& r_result);
 bool MCFiltersUrlDecode(MCStringRef p_source, MCStringRef& r_result);
 
